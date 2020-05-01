271ee4add5fb3ca226cd2f6d678cb644b88c91de
wk <somnuk.wk@gmail.com>
2020-05-01 13:55:08 +0700

    Demo

--- src/MyConsole/Program.cs
+++ src/MyConsole/Program.cs
@@ -3,6 +3,7 @@
 namespace MyConsole {
     class Program {
         static void Main(string[] args) {
+            // HelloWorldGenerated.HelloWorld.SayHello();
             HelloWorldGenerated.HelloWorld.SayHello();
         }
     }
